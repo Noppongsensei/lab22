cb56088a41d294f84437b59361f5d2f66c09f218
Sensei <noppongsen@gmail.com>
2024-02-26 21:55:23 +0700

    lab22

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input numbers to find average.\n";
+        return 0;
+    }
+    double sum=0;
+    for(int i=1;i<argc;i++){
+        sum+=atoi(argv[i]);
+    }
+
+    cout << "---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = " <<  sum/(argc-1) << endl;
+    cout << "---------------------------------\n";
+
+    return 0;
+}
--- calculator.cpp
+++ calculator.cpp
@@ -1,8 +1,64 @@
 #include <windows.h>
+#include <cstdio>
+HWND text,button1,button2,button3,button4,button5,a;
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
+		
+		case WM_CREATE:{
+			text = CreateWindow("STATIC"," Please input two numbers",WS_VISIBLE|WS_CHILD|WS_BORDER,35,20,180,25,hwnd,NULL,NULL,NULL);
+			button1 =CreateWindow("BUTTON","+",WS_VISIBLE|WS_CHILD|WS_BORDER,45,110,40,25,hwnd,(HMENU) 1,NULL,NULL);
+			button2=CreateWindow("BUTTON","-",WS_VISIBLE|WS_CHILD|WS_BORDER,85,110,40,25,hwnd,(HMENU) 2,NULL,NULL);
+			button3=CreateWindow("BUTTON","*",WS_VISIBLE|WS_CHILD|WS_BORDER,125,110,40,25,hwnd,(HMENU) 3,NULL,NULL);
+			button4=CreateWindow("BUTTON","/",WS_VISIBLE|WS_CHILD|WS_BORDER,165,110,40,25,hwnd,(HMENU) 4,NULL,NULL);
+			button5=CreateWindow("EDIT","",WS_BORDER|WS_CHILD|WS_VISIBLE,35,50,150,25,hwnd,NULL,NULL,NULL);
+			a=CreateWindow("EDIT","",WS_BORDER|WS_CHILD|WS_VISIBLE,35,80,150,25,hwnd,NULL,NULL,NULL);
+    		SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)CreateSolidBrush(RGB(250,1, 20)));
+		}
+		case WM_COMMAND: {
+
+			switch (LOWORD(wParam)) { 
+				case 1:{
+				char num1[256], num2[256],buffer[256];
+				GetWindowText(button5, num1, 256);
+				GetWindowText(a, num2, 256);
+				double Result1= atof(num1) + atof(num2);
+				sprintf(buffer, "Result = %.2f", Result1);
+				::MessageBox(hwnd,buffer,"Result", MB_OK | MB_ICONINFORMATION);
+				break;
+				}
+				case 2:{
+				char num3[256], num4[256],buffer1[256];
+				GetWindowText(button5, num3, 256);
+				GetWindowText(a, num4, 256);
+				double Result2= atof(num3) - atof(num4);
+				sprintf(buffer1, "Result = %.2f", Result2);
+				::MessageBox(hwnd,buffer1,"Result", MB_OK | MB_ICONINFORMATION);
+				break;
+				}
+				case 3:{
+				char num5[256], num6[256],buffer2[256];
+				GetWindowText(button5, num5, 256);
+				GetWindowText(a, num6, 256);
+				double Result3= atof(num5) * atof(num6);
+				sprintf(buffer2, "Result = %.2f", Result3);
+				::MessageBox(hwnd,buffer2,"Result", MB_OK | MB_ICONINFORMATION);
+				break;
+				}
+				case 4:{
+				char num7[256], num8[256],buffer3[256];
+				GetWindowText(button5, num7, 256);
+				GetWindowText(a, num8, 256);
+				double Result4= atof(num7) / atof(num8);
+				sprintf(buffer3, "Result = %.2f", Result4);
+				::MessageBox(hwnd,buffer3,"Result", MB_OK | MB_ICONINFORMATION);
+				break;
+				}
+			}
+			
+			break;
+		}
 		
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
@@ -41,11 +97,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
